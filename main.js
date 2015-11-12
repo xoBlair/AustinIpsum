716cb9811fd276cbd8b88105a9753f8402b0fe8d
Blair Johnson <helloblairj@gmail.com>
2015-11-11 18:41:02 -0600

    fixed divs

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-var foodWords = ["Gordoughs was voted best foodtruck in 2014. ", "Breakfast tacos for days. ", "This one time I saw a queso river. ", "Coffee coffee coffee espresso coffee beer. ", "There's never enough queso. ", "Avocado margs. ", "Shiner tastes like water. ", ];
-var funWords = ["Bro ... ", "Alrighty alrighty alright.  ", "Sorry, not sorry. ", "RIP Leslie. " , "Brunchhhhh. ", "Ugh, traffic sucks. ", "LOL, I'm @ ACL! ", "I barely remember SXSW. ", "Just ordered an Uber. ", "I love General Assembly Austin! ", "Download PackStack for iOS!!! ", ];
+var foodWords = ["If Anthony Bourdain can be swooned by Gourdough&#39;s warm, fresh, holy-shit-delicious donuts, you should expect to fall in love with this little Austin treasure, too.   ",
+  "I want a big fat donut.   ", "Breakfast tacos for days.   ", "This one time I saw a bird covered in queso and wanted to cry.   ",
+   "I had brunch plans but.. mimosas got me mimosted.   ", 
+  "Venti iced skinny hazelnut macchiato, sugar-free syrup, extra shot, light ice, no whip.   ",
+ "There's never enough queso.   ", "Frozen avocado margarita with sugar and chili-lime salt.   ", 
+ "Pineapple basil lime lemon watermelon infused water.  ", "Breaking news: Bluebell Ice Cream is back in stock.   ",
+  "Grande, iced, sugar-free, vanilla latte with soy milk.   ", 
+  "Texas-fried turkey breast with award-winning oaxacan mole sauce, mexican rice, avocado, quest fresco, and fresh cilantro - served on a flour tortilla.   ", ];
+
+
+var funWords = ["Bro...   ", "BRO!   ", "Alrighty alrighty alright.    ",
+ "Sorry, not sorry.   ", "RIP Leslie.   " , "LOL, I&#39;m @ ACL!   ", "I barely remember SXSW.   ",
+  "Just ordered an Uber.   ","Don&#39;t Dallas my Austin.   ", "Castle Hill Hope Outdoor gallery.   ",
+   "Pedicab Realness.   ", "Tweet that!   ", "Don&#39;t worry, I&#39;m not sick, it&#39;s allergies.   ",
+    "Wishing Ikea sold clothes.   ", "KVUE, where trust is earned.   ", "I <3 General Assembly Austin!   ",
+     "Download PackStack for iOS!!!   ", "Is that a rescue dog?    ", "The local badass is playing at Scoot Inn and we got tickets!    "];
 // var num = $("#number").val();
 var count = 0;
 
@@ -7,7 +21,7 @@ function chooseParagraph(number){
 	console.log(number);
 	if (count == 1){ 
 		$("#paragraph").text("");
-		var nonRepeat = [];
+		var nonRepeating = [];
 		for (var i = 0; i < number; i++){
 			checkWord();
 			function checkWord(){
@@ -16,7 +30,7 @@ function chooseParagraph(number){
 				var randomIndex = Math.floor(randomNum);
 				var randomWord = foodWords[randomIndex];
 				var isUnique = true;
-				nonRepeat.forEach(function(existingWord){
+				nonRepeating.forEach(function(existingWord){
 					if (existingWord == randomWord)
 					{ 
 						isUnique = false;
@@ -24,7 +38,7 @@ function chooseParagraph(number){
 				});
 				if (isUnique == true)
 				{
-					nonRepeat.push(randomWord);
+					nonRepeating.push(randomWord);
 					$("#paragraph").append(randomWord);
 				}
 				else if (isUnique == false)
