3b3aefeee319873bfb17fc4e625e3f6f477aefbd
Blair Johnson <helloblairj@gmail.com>
2015-11-12 13:27:53 -0600

    before branch creation save

--- main.js
+++ main.js
@@ -1,24 +1,13 @@
-var foodWords = ["If Anthony Bourdain can be swooned by Gourdough&#39;s warm, fresh, holy-shit-delicious donuts, you should expect to fall in love with this little Austin treasure, too.   "  ,
-  "I want a big fat donut.   "  , "Breakfast tacos for days.   "  , "This one time I saw a bird covered in queso at Torchy&#39;s and wanted to cry.   "  ,
-   "I had brunch plans but..   "  , 
-  "Venti iced skinny hazelnut macchiato, sugar-free syrup, extra shot, light ice, no whip.   "  ,
- "There's never enough queso.   "  , "Frozen avocado margarita with sugar and chili-lime salt.   "  , 
- "Pineapple basil lime lemon watermelon infused water.  "  ,  "Breaking news: Bluebell Ice Cream is back in stock.   "  ,
-  "Grande, iced, sugar-free, vanilla latte with soy milk.   "  , 
-  "Texas-fried turkey breast with award-winning oaxacan mole sauce, mexican rice, avocado, quest fresco, and fresh cilantro - served on a flour tortilla.   "  , ];
 
+// 11
+var foodWords = ["If Anthony Bourdain can be swooned by Gourdough&#39;s warm, fresh, holy-shit-delicious donuts, you should expect to fall in love with this little Austin treasure, too.   ", "I want a big fat donut.   ", "Breakfast tacos for days.   ", "This one time I saw a bird covered in queso at Torchy&#39;s and wanted to cry.   ", "I had brunch plans but..   ", "There's never enough queso.   ", "Frozen avocado margarita with sugar and chili-lime salt.   ", "Pineapple basil lime lemon watermelon infused water.  ","Breaking news: Bluebell Ice Cream is back in stock.   ", "Grande, iced, sugar-free, vanilla latte with soy milk.   ", "Texas-fried turkey breast with award-winning oaxacan mole sauce, mexican rice, avocado, quest fresco, and fresh cilantro - served on a flour tortilla.   "];
 
-var funWords = ["Bro...   ", "BRO!   ", "Alrighty alrighty alright.    ",
- "Sorry, not sorry.   ", "RIP Leslie.   " , "LOL, I&#39;m @ ACL!   ", "I barely remember SXSW.   ",
-  "Just ordered an Uber.   ","Don&#39;t Dallas my Austin.   ", "Castle Hill Hope Outdoor gallery.   ",
-   "Pedicab Realness.   ", "Tweet that!   ", "Don&#39;t worry, I&#39;m not sick, it&#39;s allergies.   ",
-    "Wishing Ikea sold clothes.   ", "KVUE, where trust is earned.   ", "I <3 General Assembly Austin!   ",
-     "Download PackStack for iOS!!!   ", "Is that a rescue dog?    ", "The local badass is playing at Scoot Inn and we got tickets!    "];
+// 12
+var funWords = ["Bro...   ", "BRO!   ", "Alrighty alrighty alright.    ", "Sorry, not sorry.   ", "RIP Leslie.   ", "LOL, I&#39;m @ ACL!   ", "I barely remember SXSW.   ", "Just ordered an Uber.   ", "Don&#39;t Dallas my Austin.   ", "Castle Hill Hope Outdoor gallery.   ", "Pedicab Realness.   ", "Tweet that!   ", "Don&#39;t worry, I&#39;m not sick, it&#39;s allergies.   ", "Wishing Ikea sold clothes.   ", "KVUE, where trust is earned.   ", "I <3 General Assembly Austin!   ", "Download PackStack for iOS!!!   ", "Is that a rescue dog?    ", "The local badass is playing at Scoot Inn and we got tickets!    "];
 // var num = $("#number").val();
 var count = 0;
 
 function chooseParagraph(number){ 
-	console.log(number);
 	if (count == 1){ 
 		$("#paragraph").text("");
 		var nonRepeating = [];
@@ -62,8 +51,8 @@ function chooseParagraph(number){
 				var randomIndex = Math.floor(randomNum);
 				var randomWord = funWords[randomIndex];
 				var isUnique = true;
-				nonRepeat.forEach(function(existingWord){
-					if (existingWord == randomWord)
+				nonRepeat.forEach(function(existingWord2){
+					if (existingWord2 == randomWord)
 					{ 
 						isUnique = false;
 					}
@@ -75,7 +64,7 @@ function chooseParagraph(number){
 				}
 				else if (isUnique == false)
 				{
-					checkWord();
+					checkWordAgain();
 				}
 			}
 		}
